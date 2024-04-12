3c22de736492b310bfca00d108d8a6128cbaf84d
papavoda <papavoda@gmail.com>
2024-04-12 23:10:21 +0300

    Remove migrations

--- django/command/runuvicorn.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from django.core.management.commands.runserver import Command as BaseCommand
-
-class Command(BaseCommand):
-    def run(self, *args, **options):
-        from uvicorn.main import run
-        run(["config.asgi:application", "--reload"])
-
